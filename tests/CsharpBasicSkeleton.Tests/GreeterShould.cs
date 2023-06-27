4ad084577f6c344a662cb3fbd7cc05d284904231
Ronny Ancorini <ronnyancorini@gmail.com>
2023-06-27 16:56:09 +0200

    chore: testing dotnet git hooks

--- tests/CsharpBasicSkeleton.Tests/GreeterShould.cs
+++ tests/CsharpBasicSkeleton.Tests/GreeterShould.cs
@@ -7,6 +7,6 @@ public class GreeterShould
 	[Test]
 	public void Greet_using_the_name()
 	{
-		Greeter.Greet("Codelier").Should().Be("Hello Codelier");
+		Greeter.Greet("Codelier").Should().Be("Hello Codelier!");
 	}
 }
