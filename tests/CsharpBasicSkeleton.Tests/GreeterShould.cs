d48da37224570c267ecfbb8fef9bc7b1d0ef28e3
Ronny Ancorini <ronnyancorini@gmail.com>
2023-06-27 11:44:06 +0200

    refactor: rename test to follow should + snake case convention

---
+++ tests/CsharpBasicSkeleton.Tests/GreeterShould.cs
@@ -0,0 +1,12 @@
+using FluentAssertions;
+
+namespace CsharpBasicSkeleton.Tests;
+
+public class GreeterShould
+{
+	[Test]
+	public void Greet_using_the_name()
+	{
+		Greeter.Greet("Codelier").Should().Be("Hello Codelier");
+	}
+}
